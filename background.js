2a118dfaa71e005a8a49aaaee711fbe32e029eb2
yacine <yacine.seffal@gmail.com>
2018-07-12 19:24:28 -0400

    continued the popup-bg communication

--- background.js
+++ background.js
@@ -1,2 +1,14 @@
-// Listen for extension icon click and make http request
-console.log('experimental background');
+// Listen for extension-icon click and make http request
+// only initiate http request if chapter is already read
+
+//listen to messages from popup.js for checkedbox
+//http request for latest chapter and change popup.html content
+
+chrome.browserAction.onClicked.addListener(function(){
+  let readCheck = document.getElementById('readCheck');
+  if (readCheck.checked){
+    console.log('box checked');
+  } else {
+    console.log('box unchecked');
+  }
+})
--- popup.js
+++ popup.js
@@ -2,12 +2,22 @@
 
 let readBtn = document.getElementById('readBtn');
 let downloadBtn = document.getElementById('downloadBtn');
+let readCheck = document.getElementById('readCheck');
+
 
 readBtn.addEventListener('click', read);
 downloadBtn.addEventListener('click', download);
 
 function read() {
   console.log('Reading in browser');
+  let httpRequest = new XMLHttpRequest();
+  httpRequest.onreadystatechange = redirectTab;
+  httpRequest.open('GET', 'https://jaiminisbox.com/reader/series/one-piece-2/');
+  httpRequest.send();
+
+  function redirectTab() {
+
+  }
 }
 
 function download() {
