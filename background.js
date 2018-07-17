7fe7837b3e96bff9e93d710de67dac4ae921daa7
yacine <yacine.seffal@gmail.com>
2018-07-17 15:33:53 -0400

    functional extension, forbidden image scraping

--- background.js
+++ background.js
@@ -1,7 +1,29 @@
+//if the popup checkbox is checked, http req to the website
+//send back the latest chapter url, the download url and the cover img
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
-  if(request.boxChecked === true){
-    sendResponse({httpRequest: "checking for latest chapter..."});
-  } else{
-    sendResponse({httpRequest: "box unchecked, no http request"})
+  if (request.boxChecked === true) {
+    let latestChapter;
+    let latestRead;
+    let latestDownload;
+
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = fillPopup;
+    xhr.open('GET', 'https://jaiminisbox.com/reader/series/one-piece-2/');
+    xhr.responseType = "document";
+    xhr.send();
+
+    function fillPopup() {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        if (xhr.status === 200) {
+          latestChapter = xhr.response;
+          latestDownload = latestChapter.querySelector(".element div a"); //a[href*='read'] not working
+          latestRead = latestChapter.querySelector(".element .title a");
+          uploadDate = latestChapter.querySelector(".element .meta_r a")
+          sendResponse({downloadLink: latestDownload.href, readLink: latestRead.href, title: latestRead.innerText, date: uploadDate.innerHTML});
+        }
+      }
+    }
+    return true; //sendResponse has to be asynchronous or variables sent are undefined
   }
 });
--- popup.js
+++ popup.js
@@ -1,43 +1,68 @@
-// depending on popup.html's checkbox, http request in background
-// in order to update the page with the latest image and links
+/* depending on popup.html's checkbox, http request in background
+in order to update the page with the latest image and links */
 
 let readBtn = document.getElementById('readBtn');
 let downloadBtn = document.getElementById('downloadBtn');
 let readCheck = document.getElementById('readCheck');
+// let coverImg = document.getElementById('latestCover'); website doesn't allow img scraping
+let titlePara = document.getElementById('title');
+let datePara = document.getElementById('date');
 
-//update checkbox according to storage
-chrome.storage.sync.get('boxStatus', function(result){
-  console.log(result.key);
-  if (result.key === true) {
-    readCheck.checked = result.key;
-  } else if (result.key === false) {
-    readCheck.checked = result.key;
+
+//update checkbox with storage
+chrome.storage.local.get('boxStatus', function(result){
+  if (typeof result.boxStatus != 'undefined') {
+    console.log('checkbox status loaded to ' + result.boxStatus);
+    readCheck.checked = result.boxStatus;
   } else {
-    console.log('no saved checkbox status');
+    console.log('checkbox status not previously stored');
   }
 });
 
-//save the checkbox status
+//save the checkbox status if changed
 readCheck.addEventListener('change', function(){
   console.log('checkbox clicked');
-  chrome.storage.sync.set({boxStatus: readCheck.checked}, function(){
+  chrome.storage.local.set({boxStatus: readCheck.checked}, function(){
     console.log('checkbox status saved to ' + readCheck.checked);
   })
 });
 
-//deal with http request in background
+//message for background http request
 chrome.runtime.sendMessage({boxChecked: readCheck.checked}, function(response){
-  console.log(response.httpRequest);
+  console.log(JSON.stringify(response));
+
+  //fill information paragraphs
+  titlePara.innerText = response.title;
+  datePara.innerText = response.date;
+
+  //add buttons click handlers
+  readBtn.addEventListener('click', read);
+  downloadBtn.addEventListener('click', download);
+
+  function read() {
+    console.log('Reading in browser');
+    chrome.tabs.create({url: response.readLink});
+  }
+
+  function download() {
+    console.log('Downloading chapter');
+    chrome.tabs.create({url: response.downloadLink});
+  }
+
+  // website detects webscraping and won't allow image retrieval
+  // let xhr = new XMLHttpRequest();
+  // xhr.onreadystatechange = changeImg;
+  // xhr.open('GET', response.readLink);
+  // xhr.responseType = "document";
+  // xhr.send();
+  //
+  // function changeImg() {
+  //   let latestCover;
+  //   if (xhr.readyState === XMLHttpRequest.DONE) {
+  //     if (xhr.status === 200) {
+  //       latestCover = (xhr.response).querySelector("#page .inner .open"); //first page img dom object
+  //       coverImg.src = latestCover.src;
+  //     }
+  //   }
+  // }
 });
-
-//button redirect links are set by the background script
-readBtn.addEventListener('click', read);
-downloadBtn.addEventListener('click', download);
-
-function read() {
-  console.log('Reading in browser');
-}
-
-function download() {
-  console.log('Downloading chapter');
-}
